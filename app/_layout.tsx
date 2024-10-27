a774cffb349f4080bb7761c7c491e20cfc02839a
Olisemedua Onwuatogwu <oonwuatogwu@csumb.edu>
2024-10-26 23:37:29 -0700

    Added prettier plugin
    Formatted my code base using prettier

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,31 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack, router } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-import BackButton from "@/assets/images/SVG/back-Button.svg"
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack, router } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import BackButton from "@/assets/images/SVG/back-Button.svg";
 
-import { useColorScheme } from '@/components/useColorScheme';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
-import { horizontalScale } from '@/utilities/TrueScale';
+import { useColorScheme } from "@/components/useColorScheme";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { horizontalScale } from "@/utilities/TrueScale";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
-
+} from "expo-router";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     "Inter-ExtraBold": require("../assets/fonts/Inter-ExtraBold.ttf"),
     "Inter-Bold": require("../assets/fonts/Inter-Bold.ttf"),
     "Inter-Light": require("../assets/fonts/Inter-Light.ttf"),
@@ -36,7 +38,6 @@ export default function RootLayout() {
     ...FontAwesome.font,
   });
 
-  
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
     if (error) throw error;
@@ -56,62 +57,94 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-
-  
-  
-
   return (
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false}} /> */}
-        <Stack.Screen name="signUp/signUpOptions" options={{  animation:"none",  
-          headerTransparent:true, headerTitle:"", headerBackTitleVisible:false,
-          headerLeft: ()=> <TouchableOpacity onPress={router.back} >
-            <BackButton></BackButton>
-          </TouchableOpacity>}} />
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      {/* <Stack.Screen name="(tabs)" options={{ headerShown: false}} /> */}
+      <Stack.Screen
+        name="signUp/signUpOptions"
+        options={{
+          animation: "none",
+          headerTransparent: true,
+          headerTitle: "",
+          headerBackTitleVisible: false,
+          headerLeft: () => (
+            <TouchableOpacity onPress={router.back}>
+              <BackButton></BackButton>
+            </TouchableOpacity>
+          ),
+        }}
+      />
 
-          <Stack.Screen name="signUp/index" options={{  animation:"none", 
-           headerTitle:"", headerBackTitleVisible:false,
-           
-           headerTransparent: true,
-           
-          headerLeft: ()=> <TouchableOpacity onPress={router.back} >
-            <BackButton style={styles.BackButton}></BackButton>
-          </TouchableOpacity>}} />
+      <Stack.Screen
+        name="signUp/index"
+        options={{
+          animation: "none",
+          headerTitle: "",
+          headerBackTitleVisible: false,
 
-          <Stack.Screen name="login/index" options={{  animation:"none", 
-           headerTitle:"", headerBackTitleVisible:false,
-           
-           headerTransparent: true,
-           
-          headerLeft: ()=> <TouchableOpacity onPress={router.back} >
-            <BackButton style={styles.BackButton}></BackButton>
-          </TouchableOpacity>}} />
+          headerTransparent: true,
 
-          
+          headerLeft: () => (
+            <TouchableOpacity onPress={router.back}>
+              <BackButton style={styles.BackButton}></BackButton>
+            </TouchableOpacity>
+          ),
+        }}
+      />
 
-          <Stack.Screen name="login/loginOptions" options={{  animation:"none",  
-          headerTransparent:true, headerTitle:"", headerBackTitleVisible:false,
-          headerLeft: ()=> <TouchableOpacity onPress={router.back} >
-            <BackButton style={styles.BackButton}></BackButton>
-          </TouchableOpacity>}} />
+      <Stack.Screen
+        name="login/index"
+        options={{
+          animation: "none",
+          headerTitle: "",
+          headerBackTitleVisible: false,
 
-          <Stack.Screen name="name" options={{  animation:"none",  
-          headerTransparent:true, headerTitle:"", headerBackTitleVisible:false,
-          headerLeft: ()=> <TouchableOpacity onPress={router.back} >
-            <BackButton style={styles.BackButton}></BackButton>
-          </TouchableOpacity>}} />
+          headerTransparent: true,
 
-          
+          headerLeft: () => (
+            <TouchableOpacity onPress={router.back}>
+              <BackButton style={styles.BackButton}></BackButton>
+            </TouchableOpacity>
+          ),
+        }}
+      />
 
-      </Stack>
+      <Stack.Screen
+        name="login/loginOptions"
+        options={{
+          animation: "none",
+          headerTransparent: true,
+          headerTitle: "",
+          headerBackTitleVisible: false,
+          headerLeft: () => (
+            <TouchableOpacity onPress={router.back}>
+              <BackButton style={styles.BackButton}></BackButton>
+            </TouchableOpacity>
+          ),
+        }}
+      />
 
+      <Stack.Screen
+        name="name"
+        options={{
+          animation: "none",
+          headerTransparent: true,
+          headerTitle: "",
+          headerBackTitleVisible: false,
+          headerLeft: () => (
+            <TouchableOpacity onPress={router.back}>
+              <BackButton style={styles.BackButton}></BackButton>
+            </TouchableOpacity>
+          ),
+        }}
+      />
+    </Stack>
   );
 }
 
-
 const styles = StyleSheet.create({
-  BackButton:{
+  BackButton: {
     marginLeft: horizontalScale(0),
-  }
-})
+  },
+});
--- app/name.tsx
+++ app/name.tsx
@@ -1,42 +1,57 @@
-import { Platform, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { getWindowSize, horizontalScale, moderateScale, verticalScale } from '@/utilities/TrueScale'
-import {useHeaderHeight} from "@react-navigation/elements"
-import { StatusBar } from 'expo-status-bar'
-import {styles} from "@/shared/css/signinLoginCss"
+import {
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import {
+  getWindowSize,
+  horizontalScale,
+  moderateScale,
+  verticalScale,
+} from "@/utilities/TrueScale";
+import { useHeaderHeight } from "@react-navigation/elements";
+import { StatusBar } from "expo-status-bar";
+import { styles } from "@/shared/css/signinLoginCss";
 
 const NamePage = () => {
-    const headerHeight = useHeaderHeight();
+  const headerHeight = useHeaderHeight();
   return (
-    
-    <View style={[styles.MainContainer, 
-    {paddingTop: verticalScale(headerHeight)}]}>
-        <StatusBar style="dark" />  
+    <View
+      style={[
+        styles.MainContainer,
+        { paddingTop: verticalScale(headerHeight) },
+      ]}
+    >
+      <StatusBar style="dark" />
 
-    <View style={styles.TopContainer}>
-      <Text style={[styles.H1,{marginBottom: verticalScale(32)}]}>Tell us about yourself</Text>
-      <Text style={[styles.regularText, {paddingBottom: verticalScale(34)}]}>
-        Please enter your legal name below</Text>
-      
-      <TextInput
-      style={styles.Input}
-        placeholder="First Name"
-      />
-      
-      <TextInput
-      style={[styles.Input, {marginTop: verticalScale(19.28)}]}
-        placeholder="Last Name"
-      />
+      <View style={styles.TopContainer}>
+        <Text style={[styles.H1, { marginBottom: verticalScale(32) }]}>
+          Tell us about yourself
+        </Text>
+        <Text
+          style={[styles.regularText, { paddingBottom: verticalScale(34) }]}
+        >
+          Please enter your legal name below
+        </Text>
 
+        <TextInput style={styles.Input} placeholder="First Name" />
+
+        <TextInput
+          style={[styles.Input, { marginTop: verticalScale(19.28) }]}
+          placeholder="Last Name"
+        />
+      </View>
+
+      <TouchableOpacity style={[styles.Btn, styles.BtnBlack]}>
+        <Text style={[styles.btnText, { color: "#ffff" }]}>Next</Text>
+      </TouchableOpacity>
     </View>
+  );
+};
 
-    <TouchableOpacity style={[styles.Btn,styles.BtnBlack]} >
-            <Text style={[styles.btnText, {color: "#ffff"}]}>Next</Text>
-          </TouchableOpacity>
-    
-    </View>
-    
-  )
-}
-
-export default NamePage
+export default NamePage;
